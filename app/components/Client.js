8e7e3ba493538c5d5896670e41c504021ea99c42
Louis DEKEISTER <louis@hopstudios.com>
2017-03-04 19:13:42 -0800

    Improve loading overlay styling

--- app/components/Client.js
+++ app/components/Client.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TasksList from './TasksList'
-import '../styles/Client.css'
+import '../styles/Client.global.css'
 
 class Client extends React.Component {
   constructor(props) {
@@ -18,11 +18,20 @@ class Client extends React.Component {
     return (
       <div className="client">
         <h2>{this.state.profile.name}</h2>
-        {this.props.clients.clientIsLoading &&
-          <div className="loading-container">
-            <h3>Loading</h3>
+
+          <div className="loading-overlay">
+            <div className="loading-wrapper">
+              <div className="loading-container">
+                <h3>Loading</h3>
+                <div className="progress">
+                  <div className="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style={{width: '100%'}}>
+                    <span className="sr-only">45% Complete</span>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        }
+
         <TasksList tasks={this.props.tasks} />
       </div>
     )
