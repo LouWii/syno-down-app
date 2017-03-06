b56783386e8376d31595cae9cc5633862d7a80cb
Louis DEKEISTER <louis@hopstudios.com>
2017-03-05 17:01:09 -0800

    add capitalize method to utils.js

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -14,3 +14,16 @@ export function fileSizeSI(a,b,c,d,e){
  return (b=Math,c=b.log,d=1e3,e=c(a)/c(d)|0,a/b.pow(d,e)).toFixed(2)
  +' '+(e?'kMGTPEZY'[--e]+'B':'Bytes')
 }
+
+/**
+ * Capitalize first word of a string
+ * from http://stackoverflow.com/a/20292655/1740412
+ * @export
+ * @param {string} [string='']
+ * @returns
+ */
+export function capitalize(string = ''){
+  return [...string].map(
+    (char, index) => index ? char : char.toUpperCase()
+  ).join('')
+}
