8462e6cd19c5dfb8ad9996414cb0320adcfe2823
Louwii <link14.online@gmail.com>
2017-03-09 19:45:48 -0800

    add logic in TasksListFilters to handle active state filter button

--- app/components/TasksListFilters.js
+++ app/components/TasksListFilters.js
@@ -3,10 +3,22 @@ import { capitalize, statusToIcon } from '../utils/utils'
 import '../styles/TasksListFilters.global.css'
 
 class TasksListFilters extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { activeFilter: 'all' }
+    this.handleStatusFilter = this.handleStatusFilter.bind(this)
+    this.renderButton = this.renderButton.bind(this)
+  }
 
   renderButton() {
+    console.log(this)
     return Object.keys(statusToIcon)
-      .map((e) => <button type="button" title={capitalize(e.replace('_', ' '))} ref={e} className="btn btn-default"><span className={statusToIcon[e]} ></span></button>);
+      .map((e) => <button key={e} type="button" title={capitalize(e.replace('_', ' '))} ref={e} className={"btn btn-"+((e===this.state.activeFilter)?"primary":"default")}><span className={statusToIcon[e]} ></span></button>);
+  }
+
+  handleStatusFilter(e) {
+    e.preventDefault()
+
   }
 
   render() {
@@ -14,7 +26,7 @@ class TasksListFilters extends React.Component {
       <div className="tasks-list-filters">
           <div className="tasks-list-filter">
             <div className="btn-group" role="group" aria-label="...">
-              <button type="button" className="btn btn-default">All</button>
+              <button type="button" ref="all" className={"btn btn-"+(('all'===this.state.activeFilter)?"primary":"default")}>All</button>
               {this.renderButton()}
             </div>
           </div>
