29a76180cb3c5c8e378cf709a7ffb6e645dd1014
Louwii <link14.online@gmail.com>
2017-03-30 20:56:22 -0700

    Change window size on startup for smaller size

--- app/main.development.js
+++ app/main.development.js
@@ -46,8 +46,8 @@ app.on('ready', async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728
+    width: 780,
+    height: 500
   });
 
   mainWindow.loadURL(`file://${__dirname}/app.html`);
