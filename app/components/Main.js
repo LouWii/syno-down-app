bb381cdd68efed97dd8dd3812829368726574d3d
Louwii <link14.online@gmail.com>
2017-03-30 21:19:47 -0700

    apply dark theme to body using js, improve css and fix tiny bottom gap

--- app/components/Main.js
+++ app/components/Main.js
@@ -7,8 +7,9 @@ import '../styles/Main.global.css';
 class Main extends Component {
 
   render() {
+    document.body.classList.toggle('dark-theme', true)
     return (
-      <div className="main dark-theme">
+      <div className="main">
         <AppBar client={this.props.client} profiles={this.props.profiles} />
         <LoadingOverlay {...this.props} />
         <Popup popup={this.props.popup} popupShow={this.props.popupShow} popupHide={this.props.popupHide} />
